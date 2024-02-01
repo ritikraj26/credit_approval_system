959bd6020a7c345716a608abd440942b01c39c0a
Ritik <ritikraj2699@outlook.com>
2024-02-01 10:45:38 +0530

    ingested customer data

---
+++ core/management/commands/ingest_customer_data.py
@@ -0,0 +1,29 @@
+# core/management/commands/ingestdata.py
+
+from django.core.management.base import BaseCommand
+from core.models import Customer, Loan
+import pandas as pd
+from decimal import Decimal
+
+class Command(BaseCommand):
+    help = 'Ingest customer data from excel files into the database'
+
+    def handle(self, *args, **options):
+        try:
+            customer_data = pd.read_excel('customer_data.xlsx')
+            loan_data = pd.read_excel('loan_data.xlsx')
+
+            print(customer_data)
+            for _, row in customer_data.iterrows():
+                Customer.objects.create(
+                    first_name=str(row.get('First Name')),
+                    last_name=str(row.get('Last Name')),
+                    age=int(row.get('Age')),
+                    phone_number=str(row.get('Phone Number')),
+                    monthly_salary=Decimal(row.get('Monthly Salary')),
+                    approved_limit=Decimal(row.get('Approved Limit')),
+                    current_debt=Decimal(row.get('Current Debt','0.0')),
+                )
+        except Exception as e:
+            print(f"Error during data ingestion: {e}")
+            raise e
---
+++ core/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Customer(models.Model):
+    customer_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    phone_number = models.CharField(max_length=20)
+    monthly_salary = models.DecimalField(max_digits=10, decimal_places=2)
+    approved_limit = models.DecimalField(max_digits=10, decimal_places=2)
+    current_debt = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+    def __str__(self):
+        return f"{self.first_name}{self.last_name}"
+    
+
+class Loan(models.Model):
+    customer_id = models.IntegerField()
+    loan_id = models.AutoField(primary_key=True)
+    loan_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    tenure = models.IntegerField()
+    interest_rate = models.DecimalField(max_digits=5, decimal_places=2)
+    monthly_repayment = models.DecimalField(max_digits=10, decimal_places=2)
+    emis_paid_on_time = models.IntegerField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return f"Loan #{self.loan_id} for {self.customer}"
+
+
