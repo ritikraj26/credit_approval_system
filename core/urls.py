e30fd55c5fd2a15238abc1a49757418765bd1203
Ritik <ritikraj2699@outlook.com>
2024-02-01 21:51:48 +0530

    created loan views

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import RegisterCustomerView, CheckEligibilityView
+from .views import RegisterCustomerView, CheckEligibilityView, CreateLoanView, ViewLoanView, ViewLoansView
 
 urlpatterns = [
     path('register_customer/', RegisterCustomerView.as_view(), name='register_customer'),
     path('check_eligibility/', CheckEligibilityView.as_view(), name='check_eligibility'),
-    # path('create_loan/', CreateLoanView.as_view(), name='create_loan'),
+    path('create_loan/', CreateLoanView.as_view(), name='create_loan'),
+    path('view_loan/<int:loan_id>/', ViewLoanView.as_view(), name='view_loan'),
+    path('view_loans/<int:customer_id>/', ViewLoansView.as_view(), name='view_loans')
 ]
--- core/views.py
+++ core/views.py
@@ -65,4 +65,96 @@ class CheckEligibilityView(APIView):
 
 class CreateLoanView(APIView):
     def post(self, request, format=None):
-        pass
+        serializer = CheckEligibilitySerializer(data=request.data)
+        if serializer.is_valid():
+            customer_id = serializer.validated_data['customer_id']
+            loan_amount = serializer.validated_data['loan_amount']
+            interest_rate = serializer.validated_data['interest_rate']
+            tenure = serializer.validated_data['tenure']
+
+            customer = Customer.objects.get(customer_id=customer_id)
+            if customer is None:
+                return Response("Customer does not exist", status=status.HTTP_400_BAD_REQUEST)
+
+            approval, corrected_interest_rate = CheckEligibilityUtils.check_loan_eligibility(customer_id, interest_rate)
+            if not approval:
+                return Response("Loan not approved", status=status.HTTP_400_BAD_REQUEST)
+
+            monthly_installment = CheckEligibilityUtils.calculate_emi(loan_amount, corrected_interest_rate, tenure)
+            loan = Loan.objects.create(
+                customer_id=customer_id,
+                loan_amount=loan_amount,
+                interest_rate=corrected_interest_rate,
+                tenure=tenure,
+                monthly_repayment=monthly_installment,
+                emis_paid_on_time=0,
+                start_date=datetime.date.today(),
+                end_date=datetime.date.today() + datetime.timedelta(days=tenure*30),
+            )
+            loan.save()
+
+            customer.current_debt += loan_amount
+            customer.save()
+
+            response_data = {
+                'loan_id': loan.loan_id,
+                'customer_id': loan.customer_id,
+                'loan_approved': approval,
+                'message': "Loan approved" if approval else "Loan not approved",
+                'monthly_installment': monthly_installment,
+            }
+
+            return Response(response_data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class ViewLoanView(APIView):
+    def get(self, request, loan_id, format=None):
+        loan = Loan.objects.get(loan_id=loan_id)
+        if loan is None:
+            return Response("Loan does not exist", status=status.HTTP_400_BAD_REQUEST)
+
+        customer = {
+            'customer_id': loan.customer_id,
+            'first_name': loan.first_name,
+            'last_name': loan.last_name,
+            'age': loan.age,
+            'phone_number': loan.phone_number
+        }
+        response_data = {
+            'loan_id': loan.loan_id,
+            'customer': customer,
+            'loan_amount': loan.loan_amount,
+            'interest_rate': loan.interest_rate,
+            'monthly_installment': loan.monthly_repayment,
+            'tenure': loan.tenure,
+        }
+
+        return Response(response_data, status=status.HTTP_200_OK)
+    
+class ViewLoansView(APIView):
+    def get(self, request, customer_id, format=None):
+        loans = Loan.objects.filter(customer_id=customer_id)
+        if loans is None:
+            return Response("Customer does not exist", status=status.HTTP_400_BAD_REQUEST)
+
+        response_data = []
+        for loan in loans:
+            customer = {
+                'customer_id': loan.customer_id,
+                'first_name': loan.first_name,
+                'last_name': loan.last_name,
+                'age': loan.age,
+                'phone_number': loan.phone_number
+            }
+            response_data.append({
+                'loan_id': loan.loan_id,
+                'customer': customer,
+                'loan_amount': loan.loan_amount,
+                'interest_rate': loan.interest_rate,
+                'monthly_installment': loan.monthly_repayment,
+                'tenure': loan.tenure,
+            })
+
+        return Response(response_data, status=status.HTTP_200_OK)
